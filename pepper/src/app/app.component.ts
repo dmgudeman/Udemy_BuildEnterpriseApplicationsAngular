830c56276114836dadef7b9ff9658e7799f6351d
davidmgudeman <davidmgudeman@gmail.com>
2017-07-08 15:01:55 -0700

    pepper lecture 17: checking the existence of an object

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -9,7 +9,8 @@ import {
          FirebaseListObservable, 
         }                         from 'angularfire2';
 import { Observable }             from 'rxjs/Observable';
-import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
 
 @Component({
     selector: 'app-root',
@@ -20,6 +21,7 @@ export class AppComponent implements OnInit {
     title = 'app works!';
     cuisines: FirebaseListObservable<any[]>;
     restaurants: Observable<any[]>;
+    exists;
 
     constructor(
         private af: AngularFire,
@@ -27,17 +29,15 @@ export class AppComponent implements OnInit {
   
     ngOnInit () {
         this.cuisines = this.af.database.list('/cuisines');
- 
         this.restaurants = this.af.database.list('/restaurants')
-            .map(restaurants => {
-                restaurants.map( restaurant => {
-                    restaurant.featureTypes = [];
-                    for ( let f in restaurant.features ) 
-                       restaurant.featureTypes.push(this.af.database.object('/features/' + f))
-                    
-                });
-                return restaurants;
-            });
+
+        // /restaurants/1/features/1
+        this.exists = this.af.database.object('/restaurants/1/features/1')
+
+        this.exists.take(1).subscribe(x => {
+           if(x && x.$value)console.log(`EXISTS`);
+           else console.log(`NOT EXISTS`);
+        });
     }
 }
 
