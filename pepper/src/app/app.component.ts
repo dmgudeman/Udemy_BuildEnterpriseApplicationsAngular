dab2a7b95b9a23df88e5c6ab37ff102e8ab2d3b0
davidmgudeman <davidmgudeman@gmail.com>
2017-07-09 11:02:53 -0700

    pepper Lecture 26: Facebook Authentication showing displayname and photoURL

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -7,12 +7,21 @@ import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
+    displayName;
+    photoURL;
     constructor( private af: AngularFire) {
     }
   
     ngOnInit () {
-
+        this.af.auth.subscribe(authState => {
+            if(!authState){
+                this.displayName =  null;
+                this.photoURL = null;
+                return;
+            }
+            this.displayName = authState.auth.displayName;
+            this.photoURL = authState.auth.photoURL;
+       });
     }
 
     login() {
@@ -21,7 +30,7 @@ export class AppComponent implements OnInit {
             method: AuthMethods.Popup
         }).then(authState => {
             console.log(`AFTER LOGIN ${authState}`);
-        })
+        });
     }
 
     logout() {
