3ff7278bac5673fd78ec854c4c199e80efb73a18
davidmgudeman <davidmgudeman@gmail.com>
2017-07-09 15:05:53 -0700

    pepper Lecture 30: Email/Password Authentication

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -10,46 +10,38 @@ import { Http }        from '@angular/http';
 export class AppComponent implements OnInit {
     displayName;
     photoURL;
+    error;
     constructor( private af: AngularFire,
                  private http: Http) {
     }
   
     ngOnInit () {
         this.af.auth.subscribe(authState => {
-            if(!authState){
-                this.displayName =  null;
-                this.photoURL = null;
-                return;
-            }
-
-            let userRef = this.af.database.object('/users/' + authState.uid);
-            userRef.subscribe(user => {
-                console.log("authState.facebook.uid", authState.facebook.uid);
-               let url = `https://graph.facebook.com/v2.8/${authState.facebook.uid}?fields=first_name,last_name&access_token=${user.accessToken}`
-               this.http.get(url).subscribe(response => {
-                   let user = response.json();
-                   userRef.update({
-                       firstName: user.first_name,
-                       lastName: user.last_name
-                   })
-               })
-            })
-           
-            this.displayName = authState.auth.displayName;
-            this.photoURL = authState.auth.photoURL;
        });
     }
-
+    
+    register() {
+        this.af.auth.createUser({
+            email: 'sparky@gmail.com',
+            password: 'sparky'
+        })
+        .then(authState =>{
+            authState.auth.sendEmailVerification();
+        })
+        .catch( err => console.log("REGISTER_-ERROR", err))
+    }
+    
     login() {
         this.af.auth.login({
-            provider: AuthProviders.Facebook,
-            method: AuthMethods.Popup,
-            scope: ['public_profile', 'user_friends']
-        }).then((authState: any) => {
-            this.af.database.object('/users/' + authState.uid).update({
-                accessToken: authState.facebook.accessToken
-            })
-        });
+            email: 'sparky@gmail.com',
+            password: 'sparky2'
+        }, {
+            method: AuthMethods.Password,
+            provider: AuthProviders.Password
+        })
+        .then(authState => console.log("LOGIN-THEN", authState))
+        .catch(error => this.error = error.message);
+
     }
 
     logout() {
