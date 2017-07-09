f99bfb4df81a67cdce6cce19ec96440cbfece92f
davidmgudeman <davidmgudeman@gmail.com>
2017-07-09 10:06:06 -0700

    pepper Lecture 22: Updating in multiple places

--- pepper/src/app/app.component.ts
+++ pepper/src/app/app.component.ts
@@ -28,18 +28,32 @@ export class AppComponent implements OnInit {
     ) { }
   
     ngOnInit () {
-        this.cuisines = this.af.database.list('/cuisines', {
-            query: {
-                orderByValue: true
-            }
-        });
-        this.restaurants = this.af.database.list('/restaurants', {
-            query: {
-                orderByChild: 'rating',
-                equalTo: 5,
-                limitToLast: 50 
-            }
-        });
+        // this.cuisines = this.af.database.list('/cuisines', {
+        //     query: {
+        //         orderByValue: true
+        //     }
+        // });
+        // this.restaurants = this.af.database.list('/restaurants', {
+        //     query: {
+        //         orderByChild: 'rating',
+        //         equalTo: 5,
+        //         limitToLast: 50 
+        //     }
+        // });
+        
+        // WANT TO ADD A CITY IN 2 PLACES
+        // /restaurants
+        // /restaurants-by-city/camberwell
+
+        this.af.database.list('/restaurants').push({name: ''})
+            .then(x => {
+                let restaurant = {name: 'My New Restaurant'}
+
+                let update = {} // represents the nodes will we update
+                update['/restaurants/' + x.key] = restaurant;
+                update['/restaurants-by-city/camberwell/' + x.key] = restaurant;
+                this.af.database.object('/').update(update);
+            })
 
         
     }
